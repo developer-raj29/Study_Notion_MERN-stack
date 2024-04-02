2f69dac4e7b037da44f1d580e83627579c7b6858
Raj Yadav <rajyadav7047@gmail.com>
2024-04-03 00:27:19 +0530

    4th Commit

--- server/Controllers/Auth.js
+++ server/Controllers/Auth.js
@@ -1,5 +1,10 @@
 const User = require("../Models/User");
 const OTP = require("../Models/OTP");
+const bcrypt = require("bcrypt");
+const otpGenerator = require("otp-generator");
+const JWT = require("jsonwebtoken");
+
+require("dotenv").config();
 
 // send otp
 exports.sendOTP = async (req, res) => {
@@ -108,17 +113,137 @@ exports.signup = async (req, res) => {
     if (existingUser) {
       return res.status(403).json({
         success: false,
-        message: "User already registered",
+        message: "User is already registered",
       });
     }
 
     // find most recent otp stored for the user
-    // const recentOTP = await OTP.
+    const recentOTP = await OTP.findOne({ email })
+      .sort({ createAt: -1 })
+      .limit(1);
+
+    console.log("Recent OTP : ", recentOTP);
 
     // validate otp
-  } catch (error) {}
+    if (recentOTP.length == 0) {
+      // OTP Not Found
+      return res.status(400).json({
+        success: false,
+        message: "OTP is not found",
+      });
+    } else if (otp !== recentOTP.otp) {
+      // Invalid OTP
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // entry create in DB
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contactNumber: null,
+    });
+
+    // entry create in DB
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstname}${lastName}`,
+    });
+
+    // return res
+    return res.status(200).json({
+      success: true,
+      message: "User is registered Successfully",
+      user: user,
+    });
+  } catch (error) {
+    console.error("Error in Sign up: ", error);
+    res.status(500).json({
+      success: false,
+      message: "User cannot be registered. Please try again",
+      error: error.message,
+    });
+  }
 };
 
 // Login
+exports.login = async (req, res) => {
+  try {
+    // get data from req body
+    const { email, password } = req.body;
+    // valdation data
+    if (!email || !password) {
+      return res.status(403).json({
+        success: false,
+        message: "Please Fill All fields are required, Please try again",
+      });
+    }
+
+    // user check exist or not
+    const user = await User.findOne({ email }).populate("additionalDetail");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User is not registered, Please Sigup First",
+      });
+    }
+
+    // generate JWT, after password matching
+    if (await bcrypt.compare(password, user.password)) {
+      const payload = {
+        email: user.email,
+        id: user._id,
+        role: user.role,
+      };
+
+      const token = JWT.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      user.token = token;
+      user.password = undefined;
+
+      // create cookie and send response
+      const options = {
+        expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+      };
+
+      return res.cookie("token", token, options).status(200).json({
+        success: true,
+        message: "Logged In Successful",
+        token: token,
+        user,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Password is incorrect",
+      });
+    }
+  } catch (error) {
+    console.error("Login Password : ", error);
+    res.status(500).json({
+      success: false,
+      message: "Login Failure, Please try agin",
+      error: error.message,
+    });
+  }
+};
 
 // change password
+exports.changePassword = async (req, res)=>{
+    
+}
