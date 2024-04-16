6b4e63c56f404b8611663238a34e11a086b53489
Raj Yadav <rajyadav7047@gmail.com>
2024-04-16 14:06:44 +0530

    1st Commit Frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,70 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import HighlightText from "../Components/Core/HomePage/HighlightText";
+import CTAButton from "../Components/Core/HomePage/Button";
+import Banner from "../assets/Images/banner.mp4";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      {/* section 1 */}
+      <div
+        className="relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center
+      text-white justify-between"
+      >
+        <Link to={"/signup"}>
+          <div
+            className="group mx-auto p-1 rounded-full bg-richblack-800 font-bold text-richblack-200
+          transition-all duration-200 hover:scale-95 w-fit"
+          >
+            <div
+              className="flex flex-row items-center gap-1 rounded-full px-10 py-[5px]
+            transition-all duration-200 group-hover:bg-richblack-900"
+            >
+              <p>Become an Instructor</p>
+              <FaArrowRight />
+            </div>
+          </div>
+        </Link>
+
+        <div className="mt-7 text-center text-4xl font-semibold">
+          Empower Your Future with
+          <HighlightText text={" Coding Skills"} />
+        </div>
+
+        <div className="mt-4 w-[90%] text-center text-lg font-bold text-richblack-300">
+          With our online coding courses, you can learn at your own pace, from
+          anywhere in the world, and get access to a wealth of resources,
+          including hands-on projects, quizzes, and personalized feedback from
+          instructors.
+        </div>
+
+        <div className="flex flex-row gap-7 mt-8">
+          <CTAButton active={true} linkto={"/signup"}>
+            Learn More
+          </CTAButton>
+          <CTAButton active={false} linkto={"/login"}>
+            Book a Demo
+          </CTAButton>
+        </div>
+
+        <div className="mx-3 my-14 w-[85%] shadow-blue-200">
+          <video muted loop autoPlay>
+            <source src={Banner} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+
+      {/* section 2 */}
+
+      {/* section 3 */}
+
+      {/* section 4 */}
+
+      {/* footer */}
+    </div>
+  );
 };
 
 export default Home;
